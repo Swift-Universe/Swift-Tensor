6fbecc3239cc80862a20d8f0835dbbb84d6c74f5
Rahul Bhalley <rahulbhalley@protonmail.com>
2019-08-02 18:28:46 +0530

    update

--- Sources/Tensor/Tensor.swift
+++ Sources/Tensor/Tensor.swift
@@ -1,15 +1,15 @@
 //
 //  Tensor.swift
-//  
+//
 //
 //  Created by Rahul Bhalley on 01/08/19.
 //
 
-protocol Initiable {
+public protocol Initiable {
     init()
 }
 
-protocol BasicMathOps {
+public protocol BasicMathOps {
     static func + (left: Self, right: Self) -> Self
     static func - (left: Self, right: Self) -> Self
     static func * (left: Self, right: Self) -> Self
@@ -20,17 +20,15 @@ extension Int:      BasicMathOps, Initiable {}
 extension Float:    BasicMathOps, Initiable {}
 extension Double:   BasicMathOps, Initiable {}
 
-struct Tensor<T: Initiable & BasicMathOps> {
+public struct Tensor<T: Initiable & BasicMathOps> {
     var elements = [T]()
-    var size: Int {
-        return elements.count
-    }
+    var size: Int { return elements.count }
     private var axes = [Int]()
     var shape: [Int] {
         get { return self.axes }
         set { precondition(newValue != [], "Error: Shape must not be empty.") }
     }
-    var rank: Int { return shape.count }
+    public var rank: Int { return shape.count }
     /**
      Initializers:
      - `init(shape:)`: all elements are initialized with`0` of `Tensor` with `shape`
@@ -38,7 +36,7 @@ struct Tensor<T: Initiable & BasicMathOps> {
      - `init(shape:elements)`:
      - `init(elements:)`:
      */
-    init(shape axes: [Int]) {
+    public init(shape axes: [Int]) {
         self.axes = axes
         var count = 1
         var length: Int {
@@ -48,7 +46,7 @@ struct Tensor<T: Initiable & BasicMathOps> {
         elements = Array<T>(repeating: T(), count: length)
     }
     
-    init(shape axes: [Int], element: T) {
+    public init(shape axes: [Int], element: T) {
         self.axes = axes
         var count = 1
         var length: Int {
@@ -58,12 +56,12 @@ struct Tensor<T: Initiable & BasicMathOps> {
         elements = Array<T>(repeating: element, count: length)
     }
     
-    init(shape axes: [Int], elements: [T]) {
+    public init(shape axes: [Int], elements: [T]) {
         self.axes = axes
         self.elements = elements
     }
     
-    init(elements: [T]) {
+    public init(elements: [T]) {
         self.axes.append(elements.count)
         self.elements = elements
     }
@@ -73,11 +71,11 @@ struct Tensor<T: Initiable & BasicMathOps> {
      - index validation
      - Shape validation
      */
-    private func isValid(_ index: Int) -> Bool {
+    fileprivate func isValid(_ index: Int) -> Bool {
         return index == 0 && index < self.shape[0]
     }
     
-    private func isValid(_ shape: [Int]) -> Bool {
+    fileprivate func isValid(_ shape: [Int]) -> Bool {
         for index in 0..<shape.count {
             if shape[index] < 0 || shape[index] >= self.shape[index] {
                 return false
@@ -90,7 +88,7 @@ struct Tensor<T: Initiable & BasicMathOps> {
      - 1D subscipt
      - 2D subscript
      */
-    subscript(index: Int) -> T {
+    public subscript(index: Int) -> T {
         get {
             assert(isValid(index), "Error: Index is not valid.")
             return self.elements[index]
@@ -100,13 +98,13 @@ struct Tensor<T: Initiable & BasicMathOps> {
         }
     }
     
-    subscript(shape: Int...) -> Int {
+    public subscript(shape: Int...) -> T {
         get {
             assert(isValid(shape), "Error: Shape is not valid.")
             var indexValue = Double((shape[0] * self.shape[1]) + shape[1])
             for _ in 0..<shape.count - 2 {
                 var newIndexValue: Double = 1
-                for j in ..<self.shape.count - 1 {
+                for j in 0..<self.shape.count - 1 {
                     newIndexValue *= Double(self.shape[j])
                     if j == shape.count - 2 {
                         newIndexValue *= Double(shape[j + 1])
@@ -122,7 +120,7 @@ struct Tensor<T: Initiable & BasicMathOps> {
             var indexValue = Double((shape[0] * self.shape[1]) + shape[1])
             for _ in 0..<shape.count - 2 {
                 var newIndexValue: Double = 1
-                for j in ..<self.shape.count - 1 {
+                for j in 0..<self.shape.count - 1 {
                     newIndexValue *= Double(self.shape[j])
                     if j == shape.count - 2 {
                         newIndexValue *= Double(shape[j + 1])
@@ -159,7 +157,7 @@ struct Tensor<T: Initiable & BasicMathOps> {
      Computed properties to compute:
      - `transpose` of a vector or matrix
      */
-    var transpose: Tensor {
+    public var transpose: Tensor {
         assert(self.shape.count == 2, "Error: Must be a vector (shape = [1, ?] or [?, 1]) or matrix for transpose.")
         var newShape = [Int]()
         if self.isVector {
@@ -167,8 +165,8 @@ struct Tensor<T: Initiable & BasicMathOps> {
             return Tensor(shape: newShape, elements: self.elements)
         } else if self.isMatrix {
             var transposedMatrix = self
-            for row in ..<self.shape[0] {
-                for column in ..<self.shape[1] {
+            for row in 0..<self.shape[0] {
+                for column in 0..<self.shape[1] {
                     transposedMatrix[column, row] = self[row, column]
                 }
             }
@@ -188,29 +186,29 @@ extension Tensor {
      - Multiplication
      - Division
      */
-    static func + (left: Tensor, right: Tensor) -> Tensor {
+    public static func + (left: Tensor, right: Tensor) -> Tensor {
         var output = [T]()
         var outputTensor = Tensor<T>(shape: [0], elements: [T()])
         // Suffices for left and right as matrices
         if left.shape == right.shape {
-            for axis in ..<left.size {
+            for axis in 0..<left.size {
                 output.append(left.elements[axis] + right.elements[axis])
             }
             outputTensor = Tensor(shape: left.shape, elements: output)
         } else if left.isVector && right.isMatrix {
             assert(left.shape[0] == 1, "Error: For matrix and vector addition, the shape of vector must be [1, ?]. Hint: use transpose(_:)")
-            for row in ..<right.shape[0] {
+            for row in 0..<right.shape[0] {
                 // Make sure the algorithm's idea is correct
-                for column in ..<left.shape[1] {
+                for column in 0..<left.shape[1] {
                     output.append(left[0, column] + right[row, column])
                 }
             }
             outputTensor = Tensor(shape: right.shape, elements: output)
         } else if left.isMatrix && right.isVector {
             assert(right.shape[0] == 1, "Error: For matrix and vector addition, the shape of vector must be [1, ?]. Hint: use transpose(_:)")
-            for row in ..<left.shape[0] {
+            for row in 0..<left.shape[0] {
                 // Make sure the algorithm's idea is correct
-                for column in ..<right.shape[1] {
+                for column in 0..<right.shape[1] {
                     output.append(left[row, column] + right[0, column])
                 }
             }
@@ -219,29 +217,29 @@ extension Tensor {
         return outputTensor
     }
     
-    static func - (left: Tensor, right: Tensor) -> Tensor {
+    public static func - (left: Tensor, right: Tensor) -> Tensor {
         var output = [T]()
         var outputTensor = Tensor<T>(shape: [0], elements: [T()])
         // Suffices for left and right as matrices
         if left.shape == right.shape {
-            for axis in ..<left.size {
+            for axis in 0..<left.size {
                 output.append(left.elements[axis] - right.elements[axis])
             }
             outputTensor = Tensor(shape: left.shape, elements: output)
         } else if left.isVector && right.isMatrix {
             assert(left.shape[0] == 1, "Error: For matrix and vector addition, the shape of vector must be [1, ?]. Hint: use transpose(_:)")
-            for row in ..<right.shape[0] {
+            for row in 0..<right.shape[0] {
                 // Make sure the algorithm's idea is correct
-                for column in ..<left.shape[1] {
+                for column in 0..<left.shape[1] {
                     output.append(left[0, column] - right[row, column])
                 }
             }
             outputTensor = Tensor(shape: right.shape, elements: output)
         } else if left.isMatrix && right.isVector {
             assert(right.shape[0] == 1, "Error: For matrix and vector addition, the shape of vector must be [1, ?]. Hint: use transpose(_:)")
-            for row in ..<left.shape[0] {
+            for row in 0..<left.shape[0] {
                 // Make sure the algorithm's idea is correct
-                for column in ..<right.shape[1] {
+                for column in 0..<right.shape[1] {
                     output.append(left[row, column] - right[0, column])
                 }
             }
@@ -250,29 +248,29 @@ extension Tensor {
         return outputTensor
     }
     
-    static func * (left: Tensor, right: Tensor) -> Tensor {
+    public static func * (left: Tensor, right: Tensor) -> Tensor {
         var output = [T]()
         var outputTensor = Tensor<T>(shape: [0], elements: [T()])
         // Suffices for left and right as matrices
         if left.shape == right.shape {
-            for axis in ..<left.size {
+            for axis in 0..<left.size {
                 output.append(left.elements[axis] * right.elements[axis])
             }
             outputTensor = Tensor(shape: left.shape, elements: output)
         } else if left.isVector && right.isMatrix {
             assert(left.shape[0] == 1, "Error: For matrix and vector addition, the shape of vector must be [1, ?]. Hint: use transpose(_:)")
-            for row in ..<right.shape[0] {
+            for row in 0..<right.shape[0] {
                 // Make sure the algorithm's idea is correct
-                for column in ..<left.shape[1] {
+                for column in 0..<left.shape[1] {
                     output.append(left[0, column] * right[row, column])
                 }
             }
             outputTensor = Tensor(shape: right.shape, elements: output)
         } else if left.isMatrix && right.isVector {
             assert(right.shape[0] == 1, "Error: For matrix and vector addition, the shape of vector must be [1, ?]. Hint: use transpose(_:)")
-            for row in ..<left.shape[0] {
+            for row in 0..<left.shape[0] {
                 // Make sure the algorithm's idea is correct
-                for column in ..<right.shape[1] {
+                for column in 0..<right.shape[1] {
                     output.append(left[row, column] * right[0, column])
                 }
             }
@@ -281,29 +279,29 @@ extension Tensor {
         return outputTensor
     }
     
-    static func / (left: Tensor, right: Tensor) -> Tensor {
+    public static func / (left: Tensor, right: Tensor) -> Tensor {
         var output = [T]()
         var outputTensor = Tensor<T>(shape: [0], elements: [T()])
         // Suffices for left and right as matrices
         if left.shape == right.shape {
-            for axis in ..<left.size {
+            for axis in 0..<left.size {
                 output.append(left.elements[axis] / right.elements[axis])
             }
             outputTensor = Tensor(shape: left.shape, elements: output)
         } else if left.isVector && right.isMatrix {
             assert(left.shape[0] == 1, "Error: For matrix and vector addition, the shape of vector must be [1, ?]. Hint: use transpose(_:)")
-            for row in ..<right.shape[0] {
+            for row in 0..<right.shape[0] {
                 // Make sure the algorithm's idea is correct
-                for column in ..<left.shape[1] {
+                for column in 0..<left.shape[1] {
                     output.append(left[0, column] / right[row, column])
                 }
             }
             outputTensor = Tensor(shape: right.shape, elements: output)
         } else if left.isMatrix && right.isVector {
             assert(right.shape[0] == 1, "Error: For matrix and vector addition, the shape of vector must be [1, ?]. Hint: use transpose(_:)")
-            for row in ..<left.shape[0] {
+            for row in 0..<left.shape[0] {
                 // Make sure the algorithm's idea is correct
-                for column in ..<right.shape[1] {
+                for column in 0..<right.shape[1] {
                     output.append(left[row, column] / right[0, column])
                 }
             }
@@ -319,14 +317,14 @@ extension Tensor {
  */
 /// TODO: Make `matrixProduct(_:_:)` generic.
 
-func matrixProduct(_ matrixA: Tensor<Double>, _ matrixB: Tensor<Double>) -> Tensor<Double> {
+public func matrixProduct(_ matrixA: Tensor<Double>, _ matrixB: Tensor<Double>) -> Tensor<Double> {
     assert(matrixA.shape.count == 2 && matrixB.shape.count == 2, "Error: Must be a matrix.")
     assert(matrixA.shape[1] == matrixB.shape[0], "Error: Column and row size condition not staisfied.")
     var result = [Double]()
-    for row in ..<matrixA.shape[0] {
-        for column in ..<matrixB.shape[1] {
+    for row in 0..<matrixA.shape[0] {
+        for column in 0..<matrixB.shape[1] {
             var product: Double = 0
-            for x in ..<matrixA.shape[1] {
+            for x in 0..<matrixA.shape[1] {
                 product += matrixA[row, x] * matrixB[x, column]
             }
             result.append(product)
@@ -341,10 +339,10 @@ func matrixProduct(_ matrixA: Tensor<Double>, _ matrixB: Tensor<Double>) -> Tens
  Visualization
  - `visualize(_:)`: Prints the matrix or vector
  */
-func visualize<T>(_ matrix: Tensor<T>) {
+public func visualize<T>(_ matrix: Tensor<T>) {
     assert(matrix.shape.count == 2, "Only vector (shape = [1, ?] or [?, 1]) or matrix are visualizable.")
-    for row in ..<matrix.shape[0] {
-        for column in ..<matrix.shape[1] {
+    for row in 0..<matrix.shape[0] {
+        for column in 0..<matrix.shape[1] {
             print(matrix[row, column], terminator: " ")
         }
         print()
